2c268801050d0d68782f05dc0af3a529ad3271ea
Tejas-Naik <rntejas2005@gmail.com>
2023-06-16 21:12:37 -0700

    Basic UI

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -1,7 +1,31 @@
-function App() {
+export default function App() {
   return (
-    <h1>Hello</h1>
-  );
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  )
 }
 
-export default App;
+
+
+function Logo() {
+  return <h1>🌴 Far Away 🎒</h1>
+};
+
+function Form() {
+  return <div className="add-form">
+    <h3>What do you need for your 😍 trip?</h3>
+  </div>
+};
+
+function PackingList() {
+  return <div className="list">LIST</div>
+};
+function Stats() {
+  return <footer className="stats">
+    <em>You have 6 items in your list, and you already packed X (X%)</em>
+  </footer>
+};
