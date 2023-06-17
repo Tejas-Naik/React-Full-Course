7044930757fb2f5fba5ec34141eab4da233196db
Tejas-Naik <rntejas2005@gmail.com>
2023-06-16 23:26:23 -0700

    Making the form feilds controlled (using state for input/select)

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -22,20 +24,40 @@ function Logo() {
 };
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Submitted");
+
+    if (!description) return;
+
+    const newItem = {
+      id: Date.now(), description, quantity, packed: false,
+    }
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1)
           .map(num =>
-            <option value={num} key={num}>{num}</option>)}
+            <option value={num} key={num}>{num}</option>)
+        }
       </select>
-      <input placeholder="Item..." type="text" />
+
+      <input
+        placeholder="Item..."
+        type="text"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+
       <button>Add</button>
     </form>)
 };
