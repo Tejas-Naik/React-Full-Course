be09b10fa8169108048b5e8393a1563ff079a8f1
Tejas-Naik <rntejas2005@gmail.com>
2023-06-25 23:26:56 -0700

    Practice Project

--- 06-eat-n-split/starter/challenges/multistep-form/src/App.js
+++ 06-eat-n-split/starter/challenges/multistep-form/src/App.js
@@ -1,3 +1,38 @@
+import { useState } from "react"
+
 export default function App() {
-  return <h1>Hello</h1>
+  const [step, setStep] = useState(1)
+  return (
+    <div className="app">
+      {step === 1 ? <PersonalInfoStep onStepChange={setStep} /> : ""}
+      {step === 2 ? <PlansStep /> : ""}
+    </div>)
 }
+
+function PersonalInfoStep({ onStepChange }) {
+  return <form className="personal-info">
+    <label>Name</label>
+    <input type="text" />
+
+    <label>Email</label>
+    <input type="email" />
+
+    <label>Phone</label>
+    <input type="tel" />
+
+    <button className="button" onClick={() => onStepChange(step => step + 1)}>Next</button>
+  </form>
+}
+function PlansStep({ onStepChange }) {
+  return <form className="personal-info">
+    <select>
+      <option>$49.99</option>
+      <option>$29.99</option>
+      <option>$19.99</option>
+    </select>
+
+    <button className="button" onClick={() => onStepChange(step => step - 1)}>Previous</button>
+    <button className="button" onClick={() => onStepChange(step => step + 1)}>Next</button>
+  </form>
+}
+
