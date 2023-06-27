3de5150438a42fae82def990da99f8c1c3f2df0a
Tejas-Naik <rntejas2005@gmail.com>
2023-06-26 22:30:29 -0700

    Using PropTypes so we can define which type of prop we want to get

--- 07-usepopcorn/starter/usepopcorn/src/StarRating.js
+++ 07-usepopcorn/starter/usepopcorn/src/StarRating.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { useState } from "react";
 
 const containerStyles = {
@@ -10,6 +11,15 @@ const starContainerStyle = {
     display: "flex",
 }
 
+StarRating.propTypes = {
+    maxRating: PropTypes.number,
+    defaultRating: PropTypes.number,
+    color: PropTypes.string,
+    size: PropTypes.number,
+    messages: PropTypes.array,
+    className: PropTypes.string,
+    onSetRating: PropTypes.func
+}
 
 export default function StarRating({
     maxRating = 5,
