867c79e5f75d7b5a9d081f38f966c6a1b62a7aac
Tejas-Naik <rntejas2005@gmail.com>
2023-07-08 00:55:52 -0700

    Learning the useReducer() from dics

--- 10-react-quiz/starter/src/App.js
+++ 10-react-quiz/starter/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import DateCounter from "./DateCounter-v2";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>    </div>
+      <DateCounter />
+    </div>
   );
 }
 
