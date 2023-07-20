fe94a4759d06d94e9b65d5cf8d7ab8dc6168cd5d
Tejas-Naik <rntejas2005@gmail.com>
2023-07-20 02:15:48 -0700

    Interacting with the Map

--- 11-worldwise/starter/worldwise/src/components/Map.jsx
+++ 11-worldwise/starter/worldwise/src/components/Map.jsx
@@ -1,22 +1,39 @@
-import { useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from "react-leaflet";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { useCities } from "../context/CitiesContext";
 import styles from "./Map.module.css";
+
+const flagemojiToPNG = (flag) => {
+    const codePoints = Array.from(flag, (char) => char.codePointAt(0));
+    const countryCode = codePoints
+        .map((code) => String.fromCharCode(code - 127397).toLowerCase())
+        .join('');
+
+    const flagImageUrl = `https://flagcdn.com/24x18/${countryCode}.png`;
+
+    return <img src={flagImageUrl} alt="flag" />;
+};
 
 function Map() {
     const navigate = useNavigate();
-    const [mapPosition, setMapPosition] = useState([40, 0])
+    const { cities } = useCities();
 
-    const [searchParams, setSearchParams] = useSearchParams();
-    const lat = searchParams.get("lat");
-    const lng = searchParams.get("lng");
-    console.log(lat, lng);
+    const [mapPosition, setMapPosition] = useState([40, 0])
+    const [searchParams] = useSearchParams();
+    const mapLat = searchParams.get("lat");
+    const mapLng = searchParams.get("lng");
+
+    useEffect(function () {
+        if (mapLat && mapLng) setMapPosition([mapLat, mapLng])
+    }, [mapLat, mapLng])
 
     return (
         <div className={styles.mapContainer} onClick={() => navigate("form")}>
             <MapContainer
                 center={mapPosition}
-                zoom={13}
+                // center={[mapLat, mapLng]}
+                zoom={6}
                 scrollWheelZoom={true}
                 className={styles.map}
             >
@@ -24,14 +41,35 @@ function Map() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
                 />
-                <Marker position={mapPosition}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {cities.map(city => (
+                    <Marker position={city.position} key={city.id}>
+                        <Popup>
+                            <span>{city.emoji ? flagemojiToPNG(city.emoji) : ""}</span><span>{city.cityName}</span>
+                        </Popup>
+                    </Marker>
+                ))}
+
+                <ChangeCenter position={mapPosition} />
+                <DetectClick />
             </MapContainer>
         </div>
     )
+}
+
+function ChangeCenter({ position }) {
+    const map = useMap();
+    map.setView(position);
+    return null;
+}
+
+function DetectClick() {
+    const navigate = useNavigate();
+
+    useMapEvents({
+        click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
+
+    })
+    return null;
 }
 
 export default Map;
