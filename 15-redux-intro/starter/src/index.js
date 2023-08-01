652697ed90ea846d3961c374008ce52f46adc65c
Tejas Naik <74360622+Tejas-Naik@users.noreply.github.com>
2023-08-01 06:12:49 +0000

    Creating a Redux store

--- 15-redux-intro/starter/src/index.js
+++ 15-redux-intro/starter/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// To run the code
+import "./store";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
